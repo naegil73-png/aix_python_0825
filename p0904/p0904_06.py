c3291e310f1ddb4cf4a62c3aa25bc9a5356732d0
jaehan <naegil73@gmail.com>
2026-09-04 18:03:12 +0900

    파일쓰기

--- p0904/p0904_06.py
+++ p0904/p0904_06.py
@@ -5,17 +5,19 @@
 stu = []
 with open("C:\\aaa\\test2.txt",'r',encoding='utf-8') as f:
     while True:
-        line = f.readline()
-        line = line.strip()
-        if line == "": break
+        line = f.readline() # 한줄씩 읽어라
+        line = line.strip() # 줄 앞뒤 공백 제거(엔터키)
+        if line == "": break # 내용이 없는 줄이 있으면, break
         line1 = line.split(",")
-        
-        print(line1,end="")
 
-        for i,v in enumerate(line1):
-            if 2<=i<5:
-                line[i] = 
-
-        stu.append(line)
+        for i, v in enumerate(line1):
+            if 2<=i<=5:
+                line1[i] = int(v)
+            elif i == 6:
+                line1[i] == float(v)
+        stu.append({"no":line1[0],"name":line1[1],"kor":line1[2],"eng":line1[3],"math":line1[4],"total":line1[5],"avg":line1[6]})
 
     print(stu)
+
+
+
